71e000177cbdda3bf56ef3c40edb88cfc27cf9b3
itisdanish <ardentdew@gmail.com>
2024-06-19 22:45:26 +0530

    API Update

--- src/components/Body.js
+++ src/components/Body.js
@@ -10,12 +10,13 @@ const Body = () => {
   useEffect(() => {
     fetchData();
   }, []);
-
+// https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.37240&lng=78.43780&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.37240&lng=78.43780&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.59080&lng=85.13480&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
+    console.log(json)
     setListRest(
       json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
